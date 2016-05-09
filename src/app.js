2d3e9cf122229c236aa9f9008c9a2613167296b7
csjcode <christophersstjohn@gmail.com>
2016-05-09 17:40:34 +0700

    Create store

--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
 
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
 import { TodoList } from './components';
+import reducer from './reducer';
 
-const dummyTodos = [
-  { id: 0, isDone: true,  text: 'make components' },
-  { id: 1, isDone: false, text: 'design actions' },
-  { id: 2, isDone: false, text: 'implement reducer' },
-  { id: 3, isDone: false, text: 'connect components' }
-];
+const store = createStore(reducer);
 
 render(
-  <TodoList todos={dummyTodos} />,
+  <TodoList todos={store.getState()} />,
   document.getElementById('app')
 );
