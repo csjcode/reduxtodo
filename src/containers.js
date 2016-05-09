04fad2542cbb8e10f445ced189a3e4aa8936ff57
csjcode <christophersstjohn@gmail.com>
2016-05-09 18:03:25 +0700

    Add provider, fixed typo in filename

--- src/containers.js
+++ src/containers.js
@@ -4,9 +4,14 @@ import { addTodo, toggleTodo } from './actions';
 
 export const TodoList = connect(
   function mapStateToProps(state) {
-    // ...
+    return { todos: state };
   },
+
   function mapDispatchToProps(dispatch) {
-    // ...
+    return {
+      addTodo: text => dispatch(addTodo(text)),
+      toggleTodo: id => dispatch(toggleTodo(id))
+    };
   }
+
 )(components.TodoList);
